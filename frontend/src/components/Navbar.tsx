bcfb8373f0d31689fd20e6e3f2aba70e0b3b7101
Vedang <vedangshetty21@gmail.com>
2025-08-27 14:48:08 +0530

    hurts bro

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -63,7 +63,7 @@ const NavBar: React.FC = () => {
     { name: "Events", href: "/events" },
     { name: "Leaderboard", href: "/leaderboard" },
     { name: "Team", href: "/team" },
-    { name: "Achievements 2", href: "/achievements" },
+    { name: "Achievements", href: "/achievements" },
     { name: "Contact", href: "/contact" },
   ];
 
