86d3dae0379cda0091f6fe9f67621bc3325ee50c
A Anish Bhat <144614131+Anish17Bhat@users.noreply.github.com>
2025-08-30 00:29:35 +0530

    fix: logo (x1)

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -42,13 +42,15 @@ const Navbar: React.FC = () => {
       <div className="max-w-5xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center h-16">
           {/* Logo */}
-          <Link to="/" className="flex items-center space-x-2" aria-label="Home">
+<Link to="/" className="flex items-center space-x-2" aria-label="Home">
             <img
               src={logo}
               alt="HackerEarth Logo"
-              className="w-12 h-12 rounded-full object-cover drop-shadow-xl border"
+              width={100}
+              height={100}
+              className="mt-2 mb-2 w-20 h-14 rounded-full object-cover drop-shadow-xl border"
             />
-            <Link to="/" className="flex items-center space-x-2" aria-label="Home">
+            <Link to="/" className="hidden sm:flex items-center space-x-2" aria-label="Home">
 
               <div className="flex items-center space-x-1 p-1 bg-white rounded-md shadow-md h-8">
                 <Monitor className="w-5 h-5 text-gray-800" />
