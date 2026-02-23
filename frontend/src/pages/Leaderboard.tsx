624c38ba03620f1478b21958cd75814592b484f6
Anish17Bhat <bhatanish17@gmail.com>
2026-02-23 20:35:27 +0530

    update: week 11 leaderboard

--- frontend/src/pages/Leaderboard.tsx
+++ frontend/src/pages/Leaderboard.tsx
@@ -32,6 +32,7 @@ const CONTEST_FILES: { viewKey: string; filename: string }[] = [
   { viewKey: "Week 8", filename: "weekly_contest_8_leaderboard.xlsx" },
   { viewKey: "Week 9", filename: "weekly_contest_9_leaderboard.xlsx" },
   { viewKey: "Week 10", filename: "weekly_contest_10_leaderboard.xlsx" },
+  { viewKey: "Week 11", filename: "weekly_contest_11_leaderboard_v2.xlsx" },
 ];
 
 const VIEW_OPTIONS = ["Cumulative", ...CONTEST_FILES.map(f => f.viewKey)];
