fa22aad16fcdcb4be097a06dcf0e512ba866138b
Dhanush y shetty <nnm24is067@nmamit.in>
2026-08-04 00:09:36 +0530

    Stop tracking backend build output

--- backend/dist/models/user.d.ts
+++
@@ -1,8 +0,0 @@
-import { Model } from 'sequelize';
-declare class User extends Model {
-    id: number;
-    google_id: string | null;
-    email: string;
-}
-export default User;
-//# sourceMappingURL=user.d.ts.map
